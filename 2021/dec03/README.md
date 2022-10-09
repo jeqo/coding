b092c92677b01b2174b55e844274dd1d24f743dc
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2022-10-09 21:02:37 +0100

    dec04 part 1

--- 2021/dec03/README.md
+++ 2021/dec03/README.md
@@ -67,8 +67,10 @@ by the CO2 scrubber rating.
 Both the oxygen generator rating and the CO2 scrubber rating
 are values that can be found in your diagnostic report - 
 finding them is the tricky part. 
+
 Both values are located using a similar process 
 that involves filtering out values until only one remains.
+
 Before searching for either rating value,
 start with the full list of binary numbers from your diagnostic report
 and consider just the first bit of those numbers. Then:
@@ -76,8 +78,10 @@ and consider just the first bit of those numbers. Then:
 - Keep only numbers selected by the bit criteria 
 for the type of rating value for which you are searching.
 Discard numbers which do not match the bit criteria.
+
 - If you only have one number left, stop;
 this is the rating value for which you are searching.
+
 - Otherwise, repeat the process, considering the next bit to the right.
 
 The bit criteria depends on which type of rating value you want to find:
@@ -86,6 +90,7 @@ The bit criteria depends on which type of rating value you want to find:
 determine the most common value (0 or 1) in the current bit position, 
 and keep only numbers with that bit in that position. 
 If 0 and 1 are equally common, keep values with a 1 in the position being considered.
+
 - To find CO2 scrubber rating, 
 determine the least common value (0 or 1) in the current bit position, 
 and keep only numbers with that bit in that position. 
---
+++ 2021/rust/dec04/src/main.rs
@@ -0,0 +1,164 @@
+use std::collections::HashMap;
+#[derive(Debug)]
+struct Position{
+    row: usize, 
+    col: usize,
+}
+
+impl Position {
+    pub fn new(row: usize, col: usize) -> Self {
+        Self {
+            row,
+            col,
+        }
+    }
+}
+#[derive(Debug)]
+struct Player {
+    id: usize,
+    nums: HashMap<String, Position>,
+    rows: [[bool; 5]; 5],
+    cols: [[bool; 5]; 5],
+}
+
+impl Player {
+    pub fn new(id: usize, num_capacity: usize) -> Self {
+        Self {
+            id: id,
+            nums: HashMap::with_capacity(num_capacity),
+            rows: [[false; 5]; 5],
+            cols: [[false; 5]; 5],
+        }
+    }
+
+    fn add(&mut self, line: String, row: usize) {
+        let v: Vec<&str> = line.split(" ").collect();
+        let mut col = 0;
+        for num in v {
+            if !num.is_empty() {
+                self.nums.insert(num.trim().to_string(), Position::new(row, col));
+                col += 1;
+            }
+        }
+    }
+
+    fn play(&mut self, num: String) -> bool {
+        if self.nums.contains_key(&num) {
+            //println!("{} found in {}", num, self.id);
+            match self.nums.get(&num) {
+                Some(Position{ row, col }) => { 
+                    println!("{} => row:{} col:{}", self.id, row, col);
+                    self.rows[*row][*col] = true;
+                    self.cols[*col][*row] = true;
+                    for i in 0..5 {
+                        println!("{:?}", self.rows[i]);
+                    }
+                    //let num_val = num.parse::<u32>().unwrap();
+                    let mut all = true;
+                    for a in self.rows[*row] {
+                        if !a { all = false }
+                    }
+                    if all {
+                        println!("{}", num);
+                        return true;
+                    }
+                    all = true;
+                    for a in self.cols[*col] {
+                        if !a { all = false }
+                    }
+                    if all {
+                        println!("{}", num);
+                        return true;
+                    }
+                    return false;
+                }
+                None => {}
+            }
+        }
+        return false;
+    }
+}
+
+struct Game {
+    numbers: Vec<String>,
+    players: Vec<Player>,
+}
+
+impl Game {
+    pub fn new(numbers: Vec<String>, players: Vec<Player>) -> Self {
+        Self {
+            numbers,
+            players
+        }
+    }
+
+    fn play(&mut self) {
+        for num in &self.numbers {
+            println!("Playing {}", num);
+            for player in &mut self.players {
+                if player.play(num.to_string()) {
+                    println!("Player {} won!", player.id);
+                    return;
+                }
+            }
+            println!();
+        }
+    }
+}
+
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+
+fn main() -> std::io::Result<()> {
+    println!("Dec04");
+
+    
+    //let path = "./../../dec04/test.txt";
+    let path = "./../../dec04/input.txt";
+    let f = File::open(path)?;
+    let mut reader = BufReader::new(f);
+
+    let mut first = String::new();
+    reader.read_line(&mut first)?;
+
+    let nums: Vec<String> = first.split(",").map(|x| x.trim().to_string()).collect();
+    println!("{:?}", nums);
+
+    let mut players: Vec<Player> = Vec::new();
+
+    loop {
+        let mut line = String::new();
+        let len = reader.read_line(&mut line)?;    
+        
+        if len == 0 { println!("EOF"); break; }
+
+        if len == 1 {
+            let mut p = Player::new(players.len(), nums.len());
+            let mut row0 = String::new();
+            reader.read_line(&mut row0)?;    
+            p.add(row0, 0);
+            let mut row1 = String::new();
+            reader.read_line(&mut row1)?;    
+            p.add(row1, 1);
+            let mut row2 = String::new();
+            reader.read_line(&mut row2)?;    
+            p.add(row2, 2);
+            let mut row3 = String::new();
+            reader.read_line(&mut row3)?;    
+            p.add(row3, 3);
+            let mut row4 = String::new();
+            reader.read_line(&mut row4)?;    
+            p.add(row4, 4);
+            players.push(p);
+        }
+    }
+
+    println!("Players {:?}", players.len());
+
+    let mut game = Game::new(nums, players);
+
+    game.play();
+
+    Ok(())
+}
