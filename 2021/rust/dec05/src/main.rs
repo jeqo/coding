5ecb901cb129ea6f1296ceb813e2298b808d5374
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2022-10-10 12:09:54 +0100

    dec05 part 2

--- 2021/rust/dec05/src/main.rs
+++ 2021/rust/dec05/src/main.rs
@@ -1,5 +1,5 @@
 #[derive(Debug,Hash,Eq,PartialEq)]
-struct Position(u32, u32);
+struct Position(i32, i32);
 
 use std::io::prelude::*;
 use std::io::BufReader;
@@ -15,9 +15,10 @@ fn main() -> std::io::Result<()> {
     let f = File::open(path)?;
     let mut reader = BufReader::new(f);
 
-    //let mut rows = HashMap::new();
-    //let mut cols = HashMap::new();
     let mut dots = HashMap::new();
+
+    let mut xmax : i32 = 0;
+    let mut ymax : i32 = 0;
 
     loop {
         let mut line = String::new();
@@ -25,10 +26,15 @@ fn main() -> std::io::Result<()> {
 
         if len == 0 { println!("EOF"); break; }
 
-        let nums: Vec<Vec<u32>> = line.trim().split(" -> ")
-            .map(|x| x.split(',').map(|y| y.parse::<u32>().unwrap()).collect())
+        let nums: Vec<Vec<i32>> = line.trim().split(" -> ")
+            .map(|x| x.split(',').map(|y| y.parse::<i32>().unwrap()).collect())
             .collect();
         assert!(nums.len() == 2);
+
+        if xmax < nums[0][0] { xmax = nums[0][0]; }
+        if xmax < nums[1][0] { xmax = nums[1][0]; }
+        if ymax < nums[0][1] { ymax = nums[0][1]; }
+        if ymax < nums[1][1] { ymax = nums[1][1]; }
 
         if nums[0][0] == nums[1][0] || nums[0][1] == nums[1][1] {
             if nums[0][0] == nums[1][0] {
@@ -86,22 +92,88 @@ fn main() -> std::io::Result<()> {
                     }
                 }
             }
+        } else { // process diagonals
+            if (nums[0][0] - nums[1][0]).abs() == (nums[0][1] - nums[1][1]).abs() {
+                println!("diagonal: {:?}", nums);
+                if nums[0][0] < nums[1][0] {
+                    if nums[0][1] < nums[1][1] { // q4
+                        let mut x = nums[0][0];
+                        let mut y = nums[0][1];
+                        while x <= nums[1][0] {
+                            let pos = Position(x, y);
+                            if let Some(inc) = dots.get_mut(&pos) {
+                                *inc += 1;
+                            } else {
+                                dots.insert(pos, 1);
+                            }
+                            y += 1;
+                            x += 1;
+                        }
+                    } else { // q1
+                        let mut x = nums[0][0];
+                        let mut y = nums[0][1];
+                        while x <= nums[1][0] {
+                            let pos = Position(x, y);
+                            if let Some(inc) = dots.get_mut(&pos) {
+                                *inc += 1;
+                            } else {
+                                dots.insert(pos, 1);
+                            }
+                            y -= 1;
+                            x += 1;
+                        }
+                    }
+                } else {
+                    if nums[0][1] < nums[1][1] { //q3
+                        let mut x = nums[0][0];
+                        let mut y = nums[0][1];
+                        while x >= nums[1][0] {
+                            let pos = Position(x, y);
+                            if let Some(inc) = dots.get_mut(&pos) {
+                                *inc += 1;
+                            } else {
+                                dots.insert(pos, 1);
+                            }
+                            y += 1;
+                            x -= 1;
+                        }
+                    } else { //q2
+                        let mut x = nums[0][0];
+                        let mut y = nums[0][1];
+                        while x >= nums[1][0] {
+                            let pos = Position(x, y);
+                            if let Some(inc) = dots.get_mut(&pos) {
+                                *inc += 1;
+                            } else {
+                                dots.insert(pos, 1);
+                            }
+                            y -= 1;
+                            x -= 1;
+                        }
+                    }
+                }
+            }
         }
-
-
     }
 
     println!("All dots");
-    for (pos, inc) in &dots {
-        println!("{} @ {:?}", inc, pos);
+    for y in 0..=ymax {
+        for x in 0..=xmax {
+            match dots.get(&Position(x, y)) {
+                Some(inc) => print!("{}", inc),
+                None => print!("."),
+            }
+        }
+        print!("\n");
     }
+
 
     println!();
 
     let mut sum = 0;
-    for (pos, inc) in &dots {
+    for (_pos, inc) in &dots {
         if *inc > 1 {
-            println!("found repeated pos @ {:?}", pos);
+            //println!("found repeated pos @ {:?}", pos);
             sum += 1;
         }
     }
