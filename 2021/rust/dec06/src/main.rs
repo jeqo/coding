eb20e0c2df3292f585db211fcb059f1bcd636927
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2022-10-15 00:52:41 +0100

    dec06 part2

--- 2021/rust/dec06/src/main.rs
+++ 2021/rust/dec06/src/main.rs
@@ -12,29 +12,68 @@ fn main() -> std::io::Result<()> {
 
     let mut l = String::new();
     let len = reader.read_line(&mut l)?;
-    let mut fishes: Vec<isize> = l
+    let mut fishes: Vec<usize> = l
         .trim()
         .split(',')
-        .map(|x| x.parse::<isize>().unwrap())
+        .map(|x| x.parse::<usize>().unwrap())
         .collect();
     println!("Line {:?} with len {}", fishes, len);
 
     //let days = 18;
-    let days = 80;
+    //let days = 80; //part 1
+
+    // part 1
+    //let days = 256;
+    //for day in 1..=days {
+    //    let mut newborns: Vec<usize> = Vec::new();
+    //    for n in fishes.iter_mut() {
+    //        match n {
+    //            0 => { *n = 6; newborns.push(8); },
+    //            _ => { *n -= 1; }
+    //        }
+    //    }
+    //    fishes.append(&mut newborns);
+    //    // println!("Day {}: {:?}", day, fishes);
+    //}
+
+    //println!("Array: {:?}", fishes);
+    //println!("Result: {}", fishes.len());
+
+    const range: usize = 9;
+    let mut pop: [u64; range] = [0; range];
+
+    println!("Population: {:?}", pop);
+    for f in fishes {
+        pop[f] += 1;
+    }
+    println!("Day 0: Population: {:?}", pop);
+
+    //let days = 18;
+    let days = 256;
     for day in 1..=days {
-        let mut newborns: Vec<isize> = Vec::new();
-        for n in fishes.iter_mut() {
-            match n {
-                0 => { *n = 6; newborns.push(8); },
-                _ => { *n -= 1; }
+        println!("Day {}: Start Population: {:?}", day, pop);
+        let mut ready = pop[0];
+        
+        for p in 0..=range - 1 {
+            if p > 0 {
+                pop[p-1] = pop[p];
+                pop[p] = 0;
             }
         }
-        fishes.append(&mut newborns);
-        println!("Day {}: {:?}", day, fishes);
+
+        pop[6] += ready;
+        pop[8] += ready;
+
+        println!("Day {}: End Population: {:?}", day, pop);
     }
 
-    println!("Array: {:?}", fishes);
-    println!("Result: {}", fishes.len());
+    let mut result = 0;
+    for r in pop {
+        result += r;
+    }
+    
+    println!("Population: {:?}", pop);
+    println!("Result: {}", result);
 
     return Ok(());
 }
