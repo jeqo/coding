d207e5b672b98d7c732605b3b795586df33f02a4
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2022-11-03 19:33:14 +0000

    draft initial java project

---
+++ 2021/java/12/src/test/java/aoc2021/day12/MainTest.java
@@ -0,0 +1,19 @@
+package aoc2021.day12;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+class MainTest {
+
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
